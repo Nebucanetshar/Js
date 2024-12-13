de87b6eb2494386d61905499191dfec1d79ef769
nebucanetshar <alhannb@hotmail.com>
2024-12-13 17:50:07 +0100

    create components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to {{pokemons[1]}}!</h1>
 
     <router-outlet />
   `,
   styles: [],
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit{
+  pokemons = ['bulbizarre','salameche'];
+  
+  ngOnInit()
+  {
+    console.table(this.pokemons)
+    this.selectPokemon('bulbizarre');
+    
+  }
+
+  selectPokemon(pokemonName: string)
+  {
+    console.log(`bien vu, ${pokemonName}`);
+  }
 }
